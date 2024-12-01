29e5b6dd4af802c1d4b3e17c78aca93b54936d4c
céline leguelvouit <celin_tfx35@MacBook-Pro.local>
2024-12-01 16:32:50 +0100

    change path img batman

--- src/assets/imgs/batman/batman-portait/batPortrait.js
+++ src/assets/imgs/batman/batman-portait/batPortrait.js
@@ -1,27 +1,27 @@
 const batPortrait = [
   {
     id: 1,
-    src: "src/assets/imgs/batman/batman-portait/bat1.jpg",
+    src: "./assets/imgs/batman/batman-portait/bat1.jpg",
   },
   {
     id: 2,
-    src: "src/assets/imgs/batman/batman-portait/bat2.jpg",
+    src: "./assets/imgs/batman/batman-portait/bat2.jpg",
   },
   {
     id: 3,
-    src: "src/assets/imgs/batman/batman-portait/bat3.png",
+    src: "./assets/imgs/batman/batman-portait/bat3.png",
   },
   {
     id: 4,
-    src: "src/assets/imgs/batman/batman-portait/bat4.jpg",
+    src: "./assets/imgs/batman/batman-portait/bat4.jpg",
   },
   {
     id: 5,
-    src: "src/assets/imgs/batman/batman-portait/bat5.webp",
+    src: "./assets/imgs/batman/batman-portait/bat5.webp",
   },
   {
     id: 6,
-    src: "src/assets/imgs/batman/batman-portait/bat6.avif",
+    src: "./assets/imgs/batman/batman-portait/bat6.avif",
   },
 ];
 
