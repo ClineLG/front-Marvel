e654199ac232780f5f08b14645927933b2a694f0
céline leguelvouit <celin_tfx35@MacBook-Pro.local>
2024-12-01 17:04:37 +0100

    change path img batman

--- src/assets/imgs/batman/batman-portait/batPortrait.js
+++ src/assets/imgs/batman/batman-portait/batPortrait.js
@@ -1,27 +1,27 @@
 const batPortrait = [
   {
     id: 1,
-    src: "./assets/imgs/batman/batman-portait/bat1.jpg",
+    src: "/src/assets/imgs/batman/batman-portait/bat1.jpg",
   },
   {
     id: 2,
-    src: "./assets/imgs/batman/batman-portait/bat2.jpg",
+    src: "/src/assets/imgs/batman/batman-portait/bat2.jpg",
   },
   {
     id: 3,
-    src: "./assets/imgs/batman/batman-portait/bat3.png",
+    src: "/src/assets/imgs/batman/batman-portait/bat3.png",
   },
   {
     id: 4,
-    src: "./assets/imgs/batman/batman-portait/bat4.jpg",
+    src: "/src/assets/imgs/batman/batman-portait/bat4.jpg",
   },
   {
     id: 5,
-    src: "./assets/imgs/batman/batman-portait/bat5.webp",
+    src: "/src/assets/imgs/batman/batman-portait/bat5.webp",
   },
   {
     id: 6,
-    src: "./assets/imgs/batman/batman-portait/bat6.avif",
+    src: "/src/assets/imgs/batman/batman-portait/bat6.avif",
   },
 ];
 
--- src/assets/imgs/batman/batmanComics/batmanComics.js
+++ src/assets/imgs/batman/batmanComics/batmanComics.js
@@ -1,23 +1,23 @@
 const batmanComics = [
   {
     id: 1,
-    src: "./assets/imgs/batman/batmanComics/batComic.jpg",
+    src: "/src/assets/imgs/batman/batmanComics/batComic.jpg",
   },
   {
     id: 2,
-    src: "./assets/imgs/batman/batmanComics/batComic2.jpg",
+    src: "/src/assets/imgs/batman/batmanComics/batComic2.jpg",
   },
   {
     id: 3,
-    src: "./assets/imgs/batman/batmanComics/batComic3.jpg",
+    src: "/src/assets/imgs/batman/batmanComics/batComic3.jpg",
   },
   {
     id: 4,
-    src: "./assets/imgs/batman/batmanComics/batComic4.jpg",
+    src: "/src/assets/imgs/batman/batmanComics/batComic4.jpg",
   },
   {
     id: 5,
-    src: "./assets/imgs/batman/batmanComics/batComic5.jpg",
+    src: "/src/assets/imgs/batman/batmanComics/batComic5.jpg",
   },
 ];
 
