115b05bf52ea0586d667cc6e82ab142cdff3fe2c
céline leguelvouit <celin_tfx35@MacBook-Pro.local>
2024-12-01 17:06:46 +0100

    change path img batman

--- src/assets/imgs/batman/batmanComics/batmanComics.js
+++ src/assets/imgs/batman/batmanComics/batmanComics.js
@@ -1,23 +1,23 @@
 const batmanComics = [
   {
     id: 1,
-    src: "/src/assets/imgs/batman/batmanComics/batComic.jpg",
+    src: "/assets/imgs/batman/batmanComics/batComic.jpg",
   },
   {
     id: 2,
-    src: "/src/assets/imgs/batman/batmanComics/batComic2.jpg",
+    src: "/assets/imgs/batman/batmanComics/batComic2.jpg",
   },
   {
     id: 3,
-    src: "/src/assets/imgs/batman/batmanComics/batComic3.jpg",
+    src: "/assets/imgs/batman/batmanComics/batComic3.jpg",
   },
   {
     id: 4,
-    src: "/src/assets/imgs/batman/batmanComics/batComic4.jpg",
+    src: "/assets/imgs/batman/batmanComics/batComic4.jpg",
   },
   {
     id: 5,
-    src: "/src/assets/imgs/batman/batmanComics/batComic5.jpg",
+    src: "/assets/imgs/batman/batmanComics/batComic5.jpg",
   },
 ];
 
